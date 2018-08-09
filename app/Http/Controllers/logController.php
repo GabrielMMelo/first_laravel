df1a236bd7146cd264be804fbbef7e2071c8647b
Picolino66 <isaiasbd@hotmail.com>
2018-08-09 17:03:00 -0300

    Adicionado função queryBD que busca todos os dados da tabela warning. Adicionado função dateDelete que verifica se a advertencia já possui 3 dias, caso possuir o botão delete some. Adicionado função visible que verifica se o usuário é Presidente Produtos Interno, assim o botão delete aparece somente para eles. Adicionado função status que verifica qual estado tá a advertencia, questão de estética para o front.

--- app/Http/Controllers/logController.php
+++ app/Http/Controllers/logController.php
@@ -5,9 +5,53 @@
 
 	use Illuminate\Support\Facades\DB;
 
+	use App\User;
+
+	use Auth;
+
+
+	//Função que retorna o nome do Status
+	function status($status){
+		switch ($status) {
+			case 1:
+				$name = "Deferida";
+				break;
+			case 2:
+				$name = "Indeferida";
+				break;
+			case 3:
+				$name = "Em processo";
+				break;
+			case 4:
+				$name = "Deletada";
+				break;
+		}
+		return $name;
+	}
+
+	//Função que torna o botão visível para Presidente e Gestão de Pessoas
+	function visible(){
+		if ((User::isPresident(Auth::user()) == 2) or (User::isInternalProcesses(Auth::user()) == 4))
+			return 1;
+		else return 0;
+	}
+
+	//Função que verifica se deram os 3 dias do botão Delete
+	function dateDelete($date){
+		$date = strtotime($date);
+
+		$dateNow= date('Y/m/d');
+		$dateNow = strtotime($dateNow);
+
+		$intervalo = ($dateNow - $date)/86400;
+		
+		if ($intervalo >= 3)
+			return 0;
+		else return 1;		
+	}
+
 	//Função que faz a busca no banco de dados
-	function queryBD($historic)
-		{
+	function queryBD($historic){
 			$i = 0;
 			//Inicializa html em -2 para saber se algum dado foi armazenado em html
 			$html[0]['id'] = -2;
@@ -20,9 +64,11 @@
 				$html[$i]['date'] = $historic->date;
 				$html[$i]['time'] = $historic->time;
 				$html[$i]['description'] = $historic->description;
-				$html[$i]['status'] = $historic->status;
+				$status = status($historic->status);
+				$html[$i]['status'] = $status;
 				$html[$i]['id_semester'] = $historic->id_semester;
-		
+				$html[$i]['dateNow'] = dateDelete($historic->date);
+				$html[$i]['visible'] = visible();
 				$i = $i + 1;
 			}
 			return ($html);
@@ -31,8 +77,7 @@
 	class logController extends Controller {
 
 		//Log Geral
-		public function general()
-		{
+		public function general(){
 			$historic = DB::select('select * from warnings');
 			$html = queryBD($historic);
 			
@@ -45,8 +90,7 @@
 		}
 
 		//Log de Cada Usuario
-		public function user(Request $request)
-		{
+		public function user(Request $request){
 			$identifier = $request->input('name');
 			$historic = DB::table('warnings')->where('penalized',$identifier)->get();
 			$html = queryBD($historic);
